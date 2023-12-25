e0c2f97b60d02462d6bf7b58d96a0ab6bd985982
doron <doron@10fs.co.il>
2023-12-25 16:48:49 +0200

    working on radar display

---
+++ ElvisOnRadar/UserControls/ELCanvas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace ElvisOnRadar.UserControls {
+  public class ELCanvas : System.Windows.Controls.Canvas {
+    public delegate void ImageRenderDelegate(DrawingContext dc);
+    public ImageRenderDelegate OnImageRender;
+
+    protected override void OnRender(DrawingContext dc) {
+      base.OnRender(dc);
+      OnImageRender?.Invoke(dc);
+    }
+  }
+}
