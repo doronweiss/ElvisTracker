ea152e5bef22bd309490c656d10d56661feb9e92
doron <doron@10fs.co.il>
2024-01-08 19:46:00 +0200

    nice

--- AvaloniaSea/Controls/RadarDisplay.axaml.cs
+++ AvaloniaSea/Controls/RadarDisplay.axaml.cs
@@ -9,12 +9,17 @@ using Avalonia;
 
 namespace AvaloniaSea.Controls {
   public partial class RadarDisplay : UserControl {
+    private const double D2R = Math.PI / 180.0;
+    private const double R2D = 1.0 / D2R;
     private double cntrX, cntrY, radius;
-    private double orientation = 0.0;
     private Bitmap? ship = null;
+
+    public static readonly StyledProperty<double> rotationProperty =
+      AvaloniaProperty.Register<RadarDisplay, double>(nameof(Azimuth));
 
     public RadarDisplay() {
       InitializeComponent();
+      AffectsRender<RadarDisplay>(rotationProperty);
     }
 
     protected override void OnInitialized() {
@@ -23,6 +28,11 @@ namespace AvaloniaSea.Controls {
       System.Diagnostics.Trace.WriteLine("Initialized");
       mainCanvas.OnImageRender += new ELCanvas.ImageRenderDelegate(this.OnCanvasRendered);
       ship = new Bitmap(AssetLoader.Open(new Uri("avares://AvaloniaSea/Assets/ship.png")));
+    }
+
+    public double Azimuth {
+      get => GetValue(rotationProperty);
+      set => SetValue(rotationProperty, value);
     }
 
     private void OnCanvasSizeChanged(object sender, SizeChangedEventArgs e) {
@@ -34,12 +44,23 @@ namespace AvaloniaSea.Controls {
     }
 
     private void OnCanvasRendered(DrawingContext dc) {
-      Pen p = new Pen(Brushes.GreenYellow, 2.0, null, PenLineCap.Flat, PenLineJoin.Miter, 10.0);
-      Pen p2 = new Pen(Brushes.GreenYellow, 1.0, DashStyle.Dash, PenLineCap.Flat, PenLineJoin.Miter, 10.0);
-      dc.DrawEllipse(null, p, new Avalonia.Point(cntrX, cntrY), radius, radius);
-      dc.DrawEllipse(null, p2, new Avalonia.Point(cntrX, cntrY), radius*0.75, radius * 0.75);
-      dc.DrawEllipse(null, p2, new Avalonia.Point(cntrX, cntrY), radius * 0.5, radius * 0.5);
-      dc.DrawEllipse(null, p2, new Avalonia.Point(cntrX, cntrY), radius * 0.25, radius * 0.25);
+      Pen pThickY = new Pen(Brushes.Yellow, 2.0, null, PenLineCap.Flat, PenLineJoin.Miter, 10.0);
+      Pen pThickGY = new Pen(Brushes.GreenYellow, 2.0, null, PenLineCap.Flat, PenLineJoin.Miter, 10.0);
+      Pen pThinGY = new Pen(Brushes.GreenYellow, 1.0, DashStyle.Dash, PenLineCap.Flat, PenLineJoin.Miter, 10.0);
+      // draw circles
+      dc.DrawEllipse(null, pThickGY, new Avalonia.Point(cntrX, cntrY), radius, radius);
+      dc.DrawEllipse(null, pThinGY, new Avalonia.Point(cntrX, cntrY), radius*0.75, radius * 0.75);
+      dc.DrawEllipse(null, pThinGY, new Avalonia.Point(cntrX, cntrY), radius * 0.5, radius * 0.5);
+      dc.DrawEllipse(null, pThinGY, new Avalonia.Point(cntrX, cntrY), radius * 0.25, radius * 0.25);
+      // draw eadial lines
+      double rot = -Azimuth*D2R;
+      dc.DrawLine(pThickY, new Point(cntrX, cntrY), new Point(cntrX + radius * Math.Sin(rot), cntrY - radius * Math.Cos(rot)));
+      rot += Math.PI / 2.0;
+      dc.DrawLine(pThinGY, new Point(cntrX, cntrY), new Point(cntrX + radius * Math.Sin(rot), cntrY - radius * Math.Cos(rot)));
+      rot += Math.PI / 2.0;
+      dc.DrawLine(pThinGY, new Point(cntrX, cntrY), new Point(cntrX + radius * Math.Sin(rot), cntrY - radius * Math.Cos(rot)));
+      rot += Math.PI / 2.0;
+      dc.DrawLine(pThinGY, new Point(cntrX, cntrY), new Point(cntrX + radius * Math.Sin(rot), cntrY - radius * Math.Cos(rot)));
       //dc.DrawImage(ship, new Rect(0, 0, 68, 355), new Rect(cntrX-10, cntrY-50, cntrX+10, cntrY+50));
       dc.DrawImage(ship, new Rect(cntrX-10, cntrY-50, 20, 100));
     }
--- AvaloniaSea/MainWindow.axaml.cs
+++ AvaloniaSea/MainWindow.axaml.cs
@@ -1,11 +1,30 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.Threading;
 
 namespace AvaloniaSea;
 
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
-        InitializeComponent();
-    }
+public partial class MainWindow : Window {
+  private double azimuth = 0.0;
+  private DispatcherTimer? timer;
+
+  public MainWindow() {
+    InitializeComponent();
+  }
+
+  public void OnWindowOpened(object? sender, EventArgs eventArgs) {
+    timer =
+      new DispatcherTimer(
+        TimeSpan.FromMilliseconds(100),
+        DispatcherPriority.Render,
+        (o, args) => {
+          azimuth += 10.0;
+          radar.Azimuth = azimuth;
+        });
+    timer.Start();
+  }
+
+  public void OnWindowClosing(object? sender, EventArgs eventArgs) {
+    timer?.Stop();
+  }
 }
